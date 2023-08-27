9b13effdf106ac9d8ffa6a21a6dad6a76489de4f
JulluisW <julluisw@gmail.com>
2023-08-27 12:14:04 +0700

    Init and Setting up project using NextJS

---
+++ app/pokemon/[id]/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+
+export default function PokemonListPage() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>POKEMON ID</div>
+    </main>
+  );
+}
---
+++ app/pokemon/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+
+export default function PokemonDetailPage() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>POKEMON Page</div>
+    </main>
+  );
+}
